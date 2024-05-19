54e192a227bc9965340def34f0732fa97656914d
Kevin Gonzalez <soyelsxbbsx@outlook.com>
2024-05-19 14:17:06 -0500

    feat: Idea inicial del main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from gestion import *
 from reportes import *
 from ventas import *
+
+
+ruta_actual = validar_ruta_main("> ")
+json_path = validar_ruta_json()
+data = opener(json_path)
+funcionejemplo = ""
+
+menu_selector(funcionejemplo,db=data)
