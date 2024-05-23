b7c354de5082efa4fe64d9177c48ff85aab94b30
Kevin Gonzalez <soyelsxbbsx@outlook.com>
2024-05-23 12:49:38 -0500

    refactor: guardado de interrupcion

--- reportes.py
+++ reportes.py
@@ -1,5 +1,18 @@
 from funciones_main import *
 from imports_gestion import *
 
-def reportes():
-    return ""
+# Formato docstring para copiar (esta linea no)
+#     '''
+#     ==> Recibe
+#     <== Devuelve
+#     '''
+
+def reportes(data_in_kwargs):
+    '''
+    Funcion para dar como argumento de longitud variable a funcion Menu_selector()
+    ==> Recibe Diccionario
+    <== Devuelve Diccionario
+    '''
+    encontrar_en_bdd()
+    res = menu_selector(mostrar_usuario_s, gestion_usuario,msg_op=3, db=data_in_kwargs)
+    return res
